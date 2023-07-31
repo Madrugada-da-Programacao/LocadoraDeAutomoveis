3ce6ed3184be32dc408eb3e17cc0a0dd8e5a3c60
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2023-07-31 15:50:09 -0300

    Adicionado Massa de Dados

---
+++ LocadoraDeAutomoveis.MassaDados/GeradorMassaDados.cs
@@ -0,0 +1,42 @@
+﻿namespace LocadoraDeAutomoveis.Infra.MassaDados
+{
+    public class GeradorMassaDados
+    {
+		//IRepositorioDisciplina repositorioDisciplina;
+		//IRepositorioMateria repositorioMateria;
+		//IRepositorioQuestao repositorioQuestao;
+
+		//ServicoTeste servicoTeste;
+
+		public GeradorMassaDados()
+        {
+
+        }
+
+
+		//public GeradorMassaDados(
+  //          IRepositorioDisciplina repositorioDisciplina,
+  //          IRepositorioMateria repositorioMateria,
+  //          IRepositorioQuestao repositorioQuestao,
+  //          ServicoTeste servicoTeste)
+  //      {
+  //          this.repositorioDisciplina = repositorioDisciplina;
+  //          this.repositorioMateria = repositorioMateria;
+  //          this.repositorioQuestao = repositorioQuestao;
+
+  //          this.servicoTeste = servicoTeste;
+  //      }
+
+        public void ConfigurarAplicacao()
+        {
+            //ConfigurarTesteMatematica();
+
+            //ConfigurarTestePortugues();
+        }
+        
+
+
+
+
+    }
+}
