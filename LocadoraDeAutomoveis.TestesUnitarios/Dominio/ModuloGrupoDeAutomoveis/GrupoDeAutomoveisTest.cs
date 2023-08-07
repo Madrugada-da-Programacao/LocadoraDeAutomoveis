54e59f2c8c6ff7f7757874c57f7b159015fec178
mm <megaferrari1@gmail.com>
2023-08-06 23:16:24 -0300

    Migrations

---
+++ LocadoraDeAutomoveis.TestesUnitarios/Dominio/ModuloGrupoDeAutomoveis/GrupoDeAutomoveisTest.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraDeAutomoveis.Dominio.ModuloGrupoDeAutomoveis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeAutomoveis.TestesUnitarios.Dominio.ModuloGrupoDeAutomoveis
+{
+    [TestClass]
+    public class GrupoDeAutomoveisTest
+    {
+        private GrupoDeAutomoveis GrupoDeAutomoveis {  get; set; }
+
+        public GrupoDeAutomoveisTest()
+        {
+            GrupoDeAutomoveis = new GrupoDeAutomoveis();
+        }
+    }
+}
